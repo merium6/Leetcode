aaf420e804c448d645c74607fdf66cfb9a51859d
Maryam Ali <123579377+merium6@users.noreply.github.com>
2024-02-19 20:58:20 +0500

    Time: 781 ms (5.00%) | Memory: 19.3 MB (41.31%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -5,7 +5,6 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             if (count(nums.begin(), nums.end(), nums[i]) == 1) {
                     num = nums[i];
-                    break;
             }
         }
         return num;
